780c75b775f5df75db4eee1bbba1932039d8cca9
Salimatou-S <mascindye2016@gmail.com>
2022-01-16 19:28:50 +0100

    amelioration

--- EXO-220113/exo1.py
+++ EXO-220113/exo1.py
@@ -2,18 +2,25 @@
 Ecrire un programme qui demande à l’utilisateur 3 nombres entiers positifs et plus petits que 40000, puis effectue la moyenne de ces trois nombres et affiche la valeur de cette moyenne.
 Le code n'effectuera pas de vérification concernant l'intervalle de 0 à 40000.""" 
 
-# premiere methode
-n = [int(i) for i in input("Entre 3 nombres entiers positifs compris entre 1 et 40000 separés par un espace:> ").split()]
-print(n)
-moyenne= float(sum(n)/3) 
-print(moyenne)
+print("Saisir 3 nombres entiers positifs et plus petits que 40000 \n")
+nombre1 = int(input('Tapez la valeur de la variable1 : '))
+nombre2 = int(input('Tapez la valeur de la variable2 : '))
+nombre3 = int(input('Tapez la valeur de la variable3 : '))
 
-# deuxieme methode
+print("voici les valeurs de variable1, de variable2 et variable3\n");
+print("première variable = ", nombre1)
+print("deuxième variable = ", nombre2)
+print("troisième variable = ", nombre3)
 
-print("Saisissez trois nombres entiers positifs plus petits que 40000")
-a = int(input("Saisissez un premier nombre :> "))
-b = int(input("Saisissez un second nombre : >"))
-c = int(input("Saisissez un troisieme nombre : >"))
+if nombre1 < 0 or nombre2 < 0 or nombre3 < 0:
+    print("Une ou plusieurs des valeurs entrées est inférieure à 0. Veuillez rentrer une autre valeur.") 
+    
+elif nombre1 > 40000 or nombre2 > 40000 or nombre3 > 40000:
+    print("Une ou plusieurs des valeurs entrées est superieure à 40000. Veuillez rentrer une autre valeur.") 
 
-moyenne = (a + b + c)/3
-print("La moyenne des 3 nombres est:", moyenne)
+else:
+    total = (nombre1 + nombre2 + nombre3)
+    print("Total = ", total)
+
+    resultat = total / 3
+    print("La moyenne des valeurs entrées est: ", resultat)
